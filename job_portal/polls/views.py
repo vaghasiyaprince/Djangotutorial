aaf9643b15698a764a791a9039bc03845fdf7266
Your Name <you@example.com>
2026-01-29 15:28:38 +0530

    [IMP] make my website

--- job_portal/polls/views.py
+++ job_portal/polls/views.py
@@ -1,7 +1,16 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    # return HttpResponse("Hello, world.")
+    return render(request , 'calc.html')
+
+def add(request):
+    val1 = request.POST['num1']
+    val2 = request.POST['num2']
+    res = int(val1) + int(val2)
+    return render(request,'result.html',{'result':res})
--- job_portal/project/settings.py
+++ job_portal/project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,4 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATICFILES_DIRS = [
+     BASE_DIR / "static",
+]
 STATIC_URL = 'static/'
