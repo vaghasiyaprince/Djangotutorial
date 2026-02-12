b73d563221568cd04321e04a8331d369dab8d750
Your Name <you@example.com>
2026-02-12 15:22:28 +0530

    [IMP] Authentication

--- job_portal/polls/views.py
+++ job_portal/polls/views.py
@@ -1,7 +1,11 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login, authenticate #add this
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm #add this
+from django.contrib.auth import login, authenticate, logout #add this
 
 # Create your views here.
 
@@ -14,3 +18,41 @@ def add(request):
     val2 = request.POST['num2']
     res = int(val1) + int(val2)
     return render(request,'result.html',{'result':res})
+
+def home(request):
+    return render(request,'home.html')
+
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("polls:home")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render (request=request, template_name="register.html", context={"register_form":form})
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("polls:home")
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="login.html", context={"login_form":form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("polls:home")
